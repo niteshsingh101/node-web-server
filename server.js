ec2a3b6f4dca7ecda65423e084b3b086777deb18
Nitesh <nitesh.singh@rsystems.com>
2018-11-02 16:17:54 +0530

    Navigation added

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send("Hello world");
+  res.render('home', {title: "Home page", description: "Welcome to home page"});
 });
 app.get('/about', (req, res) => {
   var user =[{
